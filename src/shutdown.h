f8585d59f1b0040765361eebd8a4544670de2f04
Wladimir J. van der Laan <laanwj@protonmail.com>
2020-12-08 21:49:06 +0100

    init: Signal-safe instant shutdown
    Replace the 200ms polling loop with a faster and more efficient waiting
    operation.
    
    This was tried a few times before, but given up every time because
    solutions use a condition variable which is not safe for use in signals
    as they need to be reentrant.
    
    On UNIX-ish OSes, use a safe way: a pipe. When shutdown is requested
    write a dummy byte to the pipe. Waiting for shutdown is a matter of a
    blocking read from the pipe.
    
    On Windows, there are no signals so using a condition variable is safe.

--- src/shutdown.h
+++ src/shutdown.h
@@ -6,8 +6,24 @@
 #ifndef BITCOIN_SHUTDOWN_H
 #define BITCOIN_SHUTDOWN_H
 
+/** Initialize shutdown state. This must be called before using either StartShutdown(),
+ * AbortShutdown() or WaitForShutdown(). Calling ShutdownRequested() is always safe.
+ */
+bool InitShutdownState();
+
+/** Request shutdown of the application. */
 void StartShutdown();
+
+/** Clear shutdown flag. This can be racy, so use this only in exceptional cases or for
+ * testing.
+ */
 void AbortShutdown();
+
+/** Returns true if a shutdown is requested, false otherwise. */
 bool ShutdownRequested();
+
+/** Wait for StartShutdown to be called in any thread.
+ */
+void WaitForShutdown();
 
 #endif
