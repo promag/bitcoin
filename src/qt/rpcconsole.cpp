328efdcb69eedbd3bce6d6de865cca0e3ee38374
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2021-03-20 05:06:32 +0200

    qt, rpc: Accept stop RPC even another command is executing

--- src/qt/rpcconsole.cpp
+++ src/qt/rpcconsole.cpp
@@ -881,9 +881,9 @@ void RPCConsole::setMempoolSize(long numberOfTxs, size_t dynUsage)
 
 void RPCConsole::on_lineEdit_returnPressed()
 {
-    QString cmd = ui->lineEdit->text();
+    QString cmd = ui->lineEdit->text().trimmed();
 
-    if (cmd.isEmpty() || m_is_executing) {
+    if (cmd.isEmpty()) {
         return;
     }
 
@@ -896,6 +896,16 @@ void RPCConsole::on_lineEdit_returnPressed()
         }
     } catch (const std::exception& e) {
         QMessageBox::critical(this, "Error", QString("Error: ") + QString::fromStdString(e.what()));
+        return;
+    }
+
+    if (cmd == QLatin1String("stop")) {
+        std::string dummy;
+        RPCExecuteCommandLine(m_node, dummy, cmd.toStdString());
+        return;
+    }
+
+    if (m_is_executing) {
         return;
     }
 
